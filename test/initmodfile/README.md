980cf5a0e2d2427c3ac20cf7451c2df4dd5425c0
yuanyan <yuanyan.cao@gmail.com>
2013-08-25 01:47:59 +0800

    Using logger.end for print OK message

--- test/initmodfile/README.md
+++
@@ -1,6 +0,0 @@
-### Usage
-
-```sh
-$ mod init modfile
-```
-
---
+++ test/testrunner.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var assert = require('assert');
+var path = require('path');
+var mod = require('../');
+var file = require('../lib/utils/file');
+
+var dirs = file.listdir(__dirname);
+
+// console.log(dirs)
+
+
+
