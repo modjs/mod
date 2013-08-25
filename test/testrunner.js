ad4961572884e6dbe417b7ddd5f03ab69449232e
yuanyan <yuanyan.cao@gmail.com>
2013-08-26 00:47:47 +0800

    Accept param is null

--- test/testrunner.js
+++ test/testrunner.js
@@ -3,12 +3,20 @@ var assert = require('assert');
 var path = require('path');
 var taskRunner = require('../');
 var file = require('../lib/utils/file');
+var execFile = require('child_process').execFile;
 var async = require('async');
 
 var dirs = file.listdir(__dirname);
 async.eachSeries(dirs, function (dir, done) {
     console.log(dir)
     taskRunner.run(null, dir, done);
+    execFile('bin/mod', [], {
+    	cwd: dir
+    }, function (err, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+	    done(err);
+	});
 }, function(err){
     if(err){
         throw err;
