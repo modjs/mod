aef429697229737aac37fe9f7bc677a681d9fceb
yuanyan <yuanyan.cao@gmail.com>
2013-10-17 01:05:22 +0800

    More build options based on DOM: data-group, data-stand-alone

--- test/buildnormal/dist/js/89ef9b6e.foo_baz_3_251.js
+++
@@ -1,9 +0,0 @@
-(function stand(status){
-    window['console'] && console.log(status);
-})('foo');
-(function stand(status){
-    window['console'] && console.log(status);
-})('bar');
-(function stand(status){
-    window['console'] && console.log(status);
-})('baz');
